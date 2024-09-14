23a196b94a51bf56d636cd13b28d06eb2bf39a08
deDSeC00720 <terminator.karansharma@gmail.com>
2024-09-14 16:19:25 +0530

    add matrix

---
+++ include/matrix.h
@@ -0,0 +1,50 @@
+#pragma once
+
+namespace MathEngine {
+
+    class Matrix {
+    private:
+        float matrix[3][3];
+
+    public:
+        Matrix();
+        Matrix(float, float, float, float, float, float, float, float, float);
+        Matrix(const float [3][3]);
+        Matrix(const Matrix &);
+
+    private:
+        void copy(const float [3][3]);
+        float determinant() const;
+
+    public:
+        Matrix& operator=(const Matrix &);
+
+        float* operator[](const int);
+        const float* operator[](const int) const;
+
+        Matrix operator+(const Matrix &) const;
+        Matrix& operator+=(const Matrix &);
+
+        Matrix operator-(const Matrix &) const;
+        Matrix& operator-=(const Matrix &);
+
+        Matrix operator*(const float) const;
+        Matrix& operator*=(const float);
+
+        Matrix operator/(float) const;
+        Matrix& operator/=(float);
+
+        Matrix operator*(const Matrix &) const;
+        Matrix& operator*=(const Matrix &);
+
+        bool getInverse(Matrix &) const;
+        bool inverse();
+
+        Matrix getTranspose() const;
+        Matrix& transpose();
+
+        static Matrix identity();
+
+        ~Matrix();
+    };
+}
---
+++ src/matrix.cpp
@@ -0,0 +1,185 @@
+#include "matrix.h"
+
+namespace MathEngine {
+
+    Matrix::Matrix(): matrix{0} {
+
+    }
+
+    Matrix::Matrix(float a, float b, float c, float d, float e, float f, float g, float h, float i): matrix{{a, b, c}, {d, e, f}, {g, h, i}} {
+
+    }
+
+    Matrix::Matrix(const float matrix[3][3]) {
+        copy(matrix);
+    }
+
+    Matrix::Matrix(const Matrix &mat) {
+        copy(mat.matrix);
+    }
+
+    void Matrix::copy(const float matrix[3][3]) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                this -> matrix[i][j] = matrix[i][j];
+            }
+        }
+    }
+
+    float Matrix::determinant() const {
+        return matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+    }
+
+    Matrix& Matrix::operator=(const Matrix &mat) {
+        copy(mat.matrix);
+        return *this;
+    }
+
+    float* Matrix::operator[](const int i) {
+        return matrix[i];
+    }
+
+    const float* Matrix::operator[](const int i) const {
+        return matrix[i];
+    }
+
+    Matrix Matrix::operator+(const Matrix &mat) const {
+        Matrix m;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[i][j] = matrix[i][j] + mat[i][j];
+            }
+        }
+        return m;
+    }
+
+    Matrix& Matrix::operator+=(const Matrix &mat) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] += mat[i][j];
+            }
+        }
+        return *this;
+    }
+
+    Matrix Matrix::operator-(const Matrix &mat) const {
+        Matrix m;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[i][j] = matrix[i][j] - mat[i][j]; 
+            }
+        }
+        return m;
+    }
+
+    Matrix& Matrix::operator-=(const Matrix &mat) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] -= mat[i][j];
+            }
+        }
+        return *this;
+    }
+
+    Matrix Matrix::operator*(const float k) const {
+        Matrix m;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[i][j] = matrix[i][j] * k;
+            }
+        }
+        return m;
+    }
+
+    Matrix& Matrix::operator*=(const float k) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] *= k;
+            }
+        }
+        return *this;
+    }
+
+    Matrix Matrix::operator/(float k) const {
+        k = 1 / k;
+        return (*this) * k;
+    }
+
+    Matrix& Matrix::operator/=(float k) {
+        k = 1 / k;
+        return (*this) *= k;
+    }
+
+    Matrix Matrix::operator*(const Matrix &mat) const {
+        Matrix m;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    m[i][j] += matrix[i][k] * mat[k][j];
+                }
+            }
+        }
+        return m;
+    }
+
+    Matrix& Matrix::operator*=(const Matrix &mat) {
+        (*this) = (*this) * mat;
+        return *this;
+    }
+
+    bool Matrix::getInverse(Matrix &mat) const {
+        float det = determinant();
+
+        if (det == 0.0) {
+            return false;
+        }
+
+        float oneOverDet = 1 / det;
+
+        mat[0][0] = oneOverDet * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]);
+        mat[0][1] = -oneOverDet *  (matrix[0][1] * matrix[2][2] - matrix[0][2] * matrix[2][1]);
+        mat[0][2] = oneOverDet * (matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1]);
+        mat[1][0] = -oneOverDet *  (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]);
+        mat[1][1] = oneOverDet * (matrix[0][0] * matrix[2][2] - matrix[0][2] * matrix[2][0]);
+        mat[1][2] = -oneOverDet *  (matrix[0][0] * matrix[1][2] - matrix[0][2] * matrix[1][0]);
+        mat[2][0] = oneOverDet * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+        mat[2][1] = -oneOverDet *  (matrix[0][0] * matrix[2][1] - matrix[0][1] * matrix[2][0]);
+        mat[2][2] = oneOverDet * (matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]);
+
+        return true;
+    }
+
+    bool Matrix::inverse() {
+        Matrix mat;
+        bool i = getInverse(mat);
+        (*this) = mat;
+        return i;
+    }
+
+    Matrix Matrix::getTranspose() const {
+        Matrix m;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[j][i] = matrix[i][j];
+            }
+        }
+        return m;
+    }
+
+    Matrix& Matrix::transpose() {
+        (*this) = getTranspose();
+        return *this;
+    }
+
+    Matrix Matrix::identity() {
+        return Matrix(1, 0, 0, 0, 1, 0, 0, 0, 1);
+    }
+
+    Matrix::~Matrix() {
+
+    }
+}
