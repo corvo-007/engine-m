a69fcc02bbb0532e6ab99d698dcdd2b7704a3b71
deDSeC00720 <terminator.karansharma@gmail.com>
2024-09-22 17:52:00 +0530

    add option to build both static or dynamic lib

--- include/core.h
+++ include/core.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#ifdef BUILD_DLL
-    #define MATH_ENGINE_API __declspec(dllexport)
+#ifdef DYNAMIC_LINK
+    #ifdef BUILD_DLL
+        #define MATH_ENGINE_API __declspec(dllexport)
+    #else
+        #define MATH_ENGINE_API __declspec(dllimport)
+    #endif
 #else
-    #define MATH_ENGINE_API __declspec(dllimport)
+    #define MATH_ENGINE_API
 #endif
