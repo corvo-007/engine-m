1306d22f7e23bff5ef795979a752f41afbc417fe
corvo-007 <karans00720@gmail.com>
2026-02-04 19:10:11 +0530

    remove avx512 declarations

--- src/kernels/kernel_declarations.h
+++ src/kernels/kernel_declarations.h
@@ -1,13 +1,6 @@
 #pragma once
 
 namespace EngineM::kernels {
-    namespace avx512 {
-        void matrix_add(const float (&a)[3][3], const float (&b)[3][3], float (&out)[3][3]);
-        void matrix_sub(const float (&a)[3][3], const float (&b)[3][3], float (&out)[3][3]);
-        void matrix_mul_by_k(const float (&a)[3][3], float k, float (&out)[3][3]);
-        void matrix_mul(const float (&a)[3][3], const float (&b)[3][3], float (&out)[3][3]);
-    }
-
     namespace avx2 {
         void matrix_add(const float (&a)[3][3], const float (&b)[3][3], float (&out)[3][3]);
         void matrix_sub(const float (&a)[3][3], const float (&b)[3][3], float (&out)[3][3]);
