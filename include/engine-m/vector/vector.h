56f2cd4b33c9f44003b30119d186fa4af840d0ef
corvo-007 <karans00720@gmail.com>
2026-02-28 22:24:16 +0530

    vector: move function definitions to header file making vector header only class

--- include/engine-m/vector/vector.h
+++ include/engine-m/vector/vector.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <cassert>
+#include <cmath>
+
 namespace EngineM {
-    class Quaternion;
 
     template <typename T, unsigned int N>
     struct VectorData {
@@ -69,57 +71,207 @@ namespace EngineM {
     template <typename T, unsigned int N>
     class Vector : public VectorData<T, N> {
     public:
-        Vector();
+        Vector(): VectorData<T, N>() {
 
-        Vector(T x, T y) requires (N == 2);
-        Vector(T x, T y, T z) requires (N == 3);
-        Vector(T x, T y, T z, T w) requires (N == 4);
+        }
 
-        Vector(const Vector<T, N - 1> &v, T a) requires (N > 2);
+        Vector(T x, T y) requires (N == 2): VectorData<T, N>(x, y) {
 
-        Vector(const Vector &v);
+        }
 
-        Vector& operator=(const Vector &v);
+        Vector(T x, T y, T z) requires (N == 3): VectorData<T, N>(x, y, z) {
 
-        T& operator[](unsigned int i);
-        const T& operator[](unsigned int i) const;
+        }
 
-        [[nodiscard]] Vector operator+(const Vector &v) const;
-        Vector& operator+=(const Vector &v);
+        Vector(T x, T y, T z, T w) requires (N == 4): VectorData<T, N>(x, y, z, w) {
 
-        [[nodiscard]] Vector operator-(const Vector &v) const;
-        Vector& operator-=(const Vector &v);
+        }
 
-        [[nodiscard]] Vector operator*(T k) const;
-        Vector& operator*=(T k);
+        Vector(const Vector<T, N - 1> &v, T a) requires (N > 2) {
+            for (int i = 0; i < N - 1; i++) {
+                this -> data[i] = v.data[i];
+            }
+            this -> data[N - 1] = a;
+        }
 
-        [[nodiscard]] Vector operator/(T k) const;
-        Vector& operator/=(T k);
+        Vector(const Vector &v) {
+            for (int i = 0; i < N; i++) {
+                this -> data[i] = v.data[i];
+            }
+        }
 
-        [[nodiscard]] Vector operator-() const;
+        Vector& operator=(const Vector &v) {
+            for (int i = 0; i < N; i++) {
+                this -> data[i] = v.data[i];
+            }
 
-        [[nodiscard]] T dot(const Vector &v) const;
-        [[nodiscard]] T operator*(const Vector &v) const;
+            return *this;
+        }
 
-        [[nodiscard]] T cross(const Vector &v) const requires (N == 2);
-        [[nodiscard]] T operator^(const Vector &v) const requires (N == 2);
+        T& operator[](unsigned int i) {
+            assert(i < N);
+            return this -> data[i];
+        }
+        const T& operator[](unsigned int i) const {
+            assert(i < N);
+            return this -> data[i];
+        }
 
-        [[nodiscard]] Vector cross(const Vector &v) const requires (N == 3);
-        [[nodiscard]] Vector operator^(const Vector &v) const requires (N == 3);
-        Vector& operator^=(const Vector &) requires (N == 3);
+        [[nodiscard]] Vector operator+(const Vector &v) const {
+            Vector out;
+            for (int i = 0; i < N; i++) {
+                out.data[i] = this -> data[i] + v.data[i];
+            }
+            return out;
+        }
 
-        bool operator==(const Vector &v) const;
-        bool operator!=(const Vector &v) const;
+        Vector& operator+=(const Vector &v) {
+            for (int i = 0; i < N; i++) {
+                this -> data[i] += v.data[i];
+            }
+            return *this;
+        }
 
-        [[nodiscard]] double magnitude() const;
+        [[nodiscard]] Vector operator-(const Vector &v) const {
+            Vector out;
+            for (int i = 0; i < N; i++) {
+                out.data[i] = this -> data[i] - v.data[i];
+            }
+            return out;
+        }
 
-        void normalise();
+        Vector& operator-=(const Vector &v) {
+            for (int i = 0; i < N; i++) {
+                this -> data[i] -= v.data[i];
+            }
+            return *this;
+        }
 
-        [[nodiscard]] Vector<T, 2> xy() const requires (N == 3 || N == 4);
-        [[nodiscard]] Vector<T, 2> yz() const requires (N == 3 || N == 4);
-        [[nodiscard]] Vector<T, 2> xz() const requires (N == 3 || N == 4);
+        [[nodiscard]] Vector operator*(T k) const {
+            Vector out;
+            for (int i = 0; i < N; i++) {
+                out.data[i] = this -> data[i] * k;
+            }
+            return out;
+        }
 
-        [[nodiscard]] Vector<T, 3> xyz() const requires (N == 4);
+        Vector& operator*=(T k) {
+            for (int i = 0; i < N; i++) {
+                this -> data[i] *= k;
+            }
+            return *this;
+        }
+
+        [[nodiscard]] Vector operator/(T k) const {
+            Vector out;
+            for (int i = 0; i < N; i++) {
+                out.data[i] = this -> data[i] / k;
+            }
+            return out;
+        }
+
+        Vector& operator/=(T k) {
+            for (int i = 0; i < N; i++) {
+                this -> data[i] /= k;
+            }
+            return *this;
+        }
+
+        [[nodiscard]] Vector operator-() const {
+            Vector out;
+            for (int i = 0; i < N; i++) {
+                out.data[i] = -this -> data[i];
+            }
+            return out;
+        }
+
+        [[nodiscard]] T dot(const Vector &v) const {
+            T out = 0;
+            for (int i = 0; i < N; i++) {
+                out += this -> data[i] * v.data[i];
+            }
+            return out;
+        }
+
+        [[nodiscard]] T operator*(const Vector &v) const {
+            return dot(v);
+        }
+
+        [[nodiscard]] T cross(const Vector &v) const requires (N == 2) {
+            return this -> x * v.y - this -> y * v.x;
+        }
+
+        [[nodiscard]] T operator^(const Vector &v) const requires (N == 2) {
+            return cross(v);
+        }
+
+        [[nodiscard]] Vector cross(const Vector &v) const requires (N == 3) {
+            return {this -> y * v.z - v.y * this -> z, this -> z * v.x - v.z * this -> x, this -> x * v.y - v.x * this -> y};
+        }
+
+        [[nodiscard]] Vector operator^(const Vector &v) const requires (N == 3) {
+            return cross(v);
+        }
+
+        Vector& operator^=(const Vector &v) requires (N == 3) {
+            const float x = this -> y * v.z - v.y * this -> z;
+            const float y = this -> z * v.x - v.z * this -> x;
+            this -> z = this -> x * v.y - v.x * this -> y;
+            this -> x = x;
+            this -> y = y;
+
+            return *this;
+        }
+
+        bool operator==(const Vector &v) const {
+            for (int i = 0; i < N; i++) {
+                if (this -> data[i] != v.data[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool operator!=(const Vector &v) const {
+            return !(*this == v);
+        }
+
+        [[nodiscard]] double magnitude() const {
+            double out = 0;
+
+            for (int i = 0; i < N; i++) {
+                out += this -> data[i] * this -> data[i];
+            }
+
+            return std::sqrt(out);
+        }
+
+        void normalise() {
+            double mag = magnitude();
+            if (mag == 0) {
+                return ;
+            }
+            for (int i = 0; i < N; i++) {
+                this -> data[i] /= mag;
+            }
+        }
+
+        [[nodiscard]] Vector<T, 2> xy() const requires (N == 3 || N == 4) {
+            return { this -> x, this -> y };
+        }
+
+        [[nodiscard]] Vector<T, 2> yz() const requires (N == 3 || N == 4) {
+            return { this -> y, this -> z };
+        }
+
+        [[nodiscard]] Vector<T, 2> xz() const requires (N == 3 || N == 4) {
+            Vector<double, 3> t;
+            return { this -> x, this -> z };
+        }
+
+        [[nodiscard]] Vector<T, 3> xyz() const requires (N == 4) {
+            return { this -> x, this -> y, this -> z };
+        }
 
         ~Vector() = default;
     };
